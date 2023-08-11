b49b0506886266943592e8d5ec658dd372df8f74
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-11 15:15:35 -0700

    bruh, brought the initialization to outside the loop, and got 1 ms more runtime. system is rigged.

--- Solution.java
+++ Solution.java
@@ -1,13 +1,11 @@
-import java.util.*;
 
 public class Solution {
     public void reverseString(char[] s) {
-        
+        char curVal;
         for (int index = 0; index < s.length / 2; index++) {
-            char curVal = s[index];
+            curVal = s[index];
             s[index] = s[s.length - index - 1];
             s[s.length - index - 1] = curVal;
-            System.out.println(Arrays.toString(s));
         }
     }
 }
